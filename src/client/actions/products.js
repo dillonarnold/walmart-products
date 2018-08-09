60734906253cf8afb209c3b4493e6f7c499f4bc4
Dillon Arnold <dillon.arnold@cdk.com>
2018-08-08 21:05:31 -0700

    Cleaning up duplicated code

--- src/client/actions/products.js
+++ src/client/actions/products.js
@@ -39,7 +39,7 @@ export const getNextPage = (query) => {
   return dispatch => {
     if (currentPage !== Math.ceil(totalResults / numProducts)) {
       dispatch(increasePage());
-      dispatch(searchPaginatedProducts(query));
+      dispatch(searchProducts(query, true));
     }
   };
 };
@@ -52,13 +52,13 @@ export const getPreviousPage = (query) => {
   return dispatch => {
     if (currentPage !== 1) {
       dispatch(decreasePage());
-      dispatch(searchPaginatedProducts(query));
+      dispatch(searchProducts(query, true));
     }
   };
 };
 
 // A thunk to trigger the loader and fetch the products
-export const searchProducts = (query) => {
+export const searchProducts = (query, justProducts = false) => {
 
   return dispatch => {
 
@@ -79,43 +79,15 @@ export const searchProducts = (query) => {
     dispatch(fetchingProducts());
     return fetch(`/api/product/search?query=${query}&start=${index}`)
       .then(response => response.json())
-      .then(json => dispatch(addProducts(json)))
-      .catch(e => {
-        // TODO implement error handling for when HAPI API returns an error
-      });
-  };
-};
-
-
-/**
- * This seems like duplicated code, but the Walmart search API has a weird quirk.
- * Once you hit the last page of products, the Walmart API will return the number of
- * those products as the totalResults. So if your original search returned 353 products,
- * the last page will return totalResults as 3, which isn't true. So we need to avoid using
- * that value.
- */
-export const searchPaginatedProducts = (query) => {
-
-  return dispatch => {
-
-    /**
-     * Questionable, but Dan Abramov says this is ok. Ideally this should use a selector.
-     * https://stackoverflow.com/questions/35667249/accessing-redux-state-in-an-action-creator
-     */
-    const { currentPage, numProducts } = store.getState().products;
-
-    /**
-     * Map the page to a product index. Example:
-     *
-     * 10 products per page, so page 1's index would be 1
-     * page 2's index would be 11
-     */
-    const index = numProducts === 0 ? 1 : (currentPage * numProducts) - (numProducts - 1);
-
-    dispatch(fetchingProducts());
-    return fetch(`/api/product/search?query=${query}&start=${index}`)
-      .then(response => response.json())
-      .then(json => dispatch(addPaginatedProducts(json.products)))
+      .then(json =>  {
+        // Do we only want the products, or do we also want the paginated data?
+        if (justProducts) {
+          dispatch(addPaginatedProducts(json.products));
+        }
+        else {
+          dispatch(addProducts(json));
+        }
+      })
       .catch(e => {
         // TODO implement error handling for when HAPI API returns an error
       });
